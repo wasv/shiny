ff1adc916abc7d2c0667eaa0a4c0c8c77c00e770
Billy Stevens <contact@wasv.me>
2020-06-26 23:05:23 -0400

    Builds in webview.

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,84 @@
-extern crate iced;
+extern crate actix_rt;
+extern crate actix_web;
+extern crate futures;
+extern crate mime_guess;
+extern crate rust_embed;
+extern crate web_view;
 
-use iced::{executor, Application, Command, Element, Settings, Text};
+use actix_web::{body::Body, web, App, HttpRequest, HttpResponse, HttpServer};
+use mime_guess::from_path;
+use rust_embed::RustEmbed;
+use std::{borrow::Cow, sync::mpsc, thread};
+use web_view::*;
 
-pub fn main() {
-    Hello::run(Settings::default())
+#[derive(RustEmbed)]
+#[folder = "target-wasm32/shiny/"]
+struct Asset;
+
+fn assets(req: HttpRequest) -> HttpResponse {
+    let path = if req.path() == "/" {
+        // if there is no path, return default file
+        "index.html"
+    } else {
+        // trim leading '/'
+        &req.path()[1..]
+    };
+
+    // query the file from embedded asset with specified path
+    match Asset::get(path) {
+        Some(content) => {
+            let body: Body = match content {
+                Cow::Borrowed(bytes) => bytes.into(),
+                Cow::Owned(bytes) => bytes.into(),
+            };
+            HttpResponse::Ok()
+                .content_type(from_path(path).first_or_octet_stream().as_ref())
+                .body(body)
+        }
+        None => HttpResponse::NotFound().body("404 Not Found"),
+    }
 }
 
-struct Hello;
+fn main() {
+    let (server_tx, server_rx) = mpsc::channel();
+    let (port_tx, port_rx) = mpsc::channel();
 
-impl Application for Hello {
-    type Executor = executor::Null;
-    type Message = ();
-    type Flags = ();
+    // start actix web server in separate thread
+    thread::spawn(move || {
+        let sys = actix_rt::System::new("shiny-server");
 
-    fn new(_flags: ()) -> (Hello, Command<Self::Message>) {
-        (Hello, Command::none())
-    }
+        let server = HttpServer::new(|| App::new().route("*", web::get().to(assets)))
+            .bind("127.0.0.1:0")
+            .unwrap();
 
-    fn title(&self) -> String {
-        String::from("Shiny!")
-    }
+        // we specified the port to be 0,
+        // meaning the operating system
+        // will choose some available port
+        // for us
+        // get the first bound address' port,
+        // so we know where to point webview at
+        let port = server.addrs().first().unwrap().port();
+        let server = server.start();
 
-    fn update(&mut self, _message: Self::Message) -> Command<Self::Message> {
-        Command::none()
-    }
+        let _ = port_tx.send(port);
+        let _ = server_tx.send(server);
+        let _ = sys.run();
+    });
 
-    fn view(&mut self) -> Element<Self::Message> {
-        Text::new("Everything's shiny cap'n!").into()
-    }
+    let port = port_rx.recv().unwrap();
+    let server = server_rx.recv().unwrap();
+
+    web_view::builder()
+        .title("Shiny!")
+        .content(Content::Url(format!("http://127.0.0.1:{}", port)))
+        .size(320, 480)
+        .resizable(false)
+        .debug(true)
+        .user_data(())
+        .invoke_handler(|_webview, _arg| Ok(()))
+        .run()
+        .unwrap();
+
+    // gracefully shutdown actix web server
+    let _ = server.stop(true);
 }
